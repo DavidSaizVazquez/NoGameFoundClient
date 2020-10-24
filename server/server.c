aef4d6ef2a7310c30cc5f346fd0ae4df6420db0b
dave <dave@pop-os.localdomain>
2020-10-24 12:17:21 +0200

    added CMakeLists for easier compilation

--- server/server.c
+++ server/server.c
@@ -1,15 +1,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <mysql.h>
+#include "mysql.h"
 
 
 int initMySQLServer(MYSQL *conn) {
-	int err;
 	//Creamos una conexion al servidor MYSQL
 	conn = mysql_init(NULL);
 	if (conn == NULL)
@@ -21,7 +19,7 @@ int initMySQLServer(MYSQL *conn) {
 
 	//inicializar la conexion, indicando nuestras claves de acceso
 	// al servidor de bases de datos (user,pass)
-	conn = mysql_real_connect(conn, "localhost", "root", "mysql", "GameDB", 0, NULL, 0); // CHANGE TO THE COMPUTER SETTINGS 
+	conn = mysql_real_connect(conn, "localhost", "root", "admin", "GameDB", 0, NULL, 0); // CHANGE TO THE COMPUTER SETTINGS
 	if (conn == NULL)
 	{
 		printf("Error al inicializar la conexion: %u %s\n",
@@ -31,44 +29,41 @@ int initMySQLServer(MYSQL *conn) {
 	return 0;
 }
 
-int startTCPServer(struct sockaddr_in serv_adr, int * sock_conn, int * sock_listen, int PORT) {
-	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+int startTCPServer(struct sockaddr_in *serv_adr, int * sock_conn, int * sock_listen, int PORT) {
+	if ((*sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("Error creating socket");
 		return -1;
 	}
 	printf("Socket created");
 
 
-	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
-	serv_adr.sin_family = AF_INET;
+	memset(serv_adr, 0, sizeof(*serv_adr));// inicialitza a zero serv_addr
+	serv_adr->sin_family = AF_INET;
 
 	// asocia el socket a cualquiera de las IP de la m?quina. 
 	//htonl formatea el numero que recibe al formato necesario
-	auto a=htonl(INADDR_ANY);
-	//serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr.sin_addr.s_addr = inet_addr("10.0.3.15");
+	serv_adr->sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	// establecemos el puerto de escucha
-	serv_adr.sin_port = htons(PORT);
-	if (bind(sock_listen, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) != 0) {
+	serv_adr->sin_port = htons(PORT);
+	if (bind(*sock_listen, (struct sockaddr*)&serv_adr, sizeof(*serv_adr)) != 0) {
 		printf("Error binding");
 		return -1;
 	}
 	printf("binding ended");
 
-	if (listen(sock_listen, 3) != 0) {
+	if (listen(*sock_listen, 3) != 0) {
 		printf("Error en el Listen");
 		return -1;
 	}
 	printf("listening");
-	sock_conn = accept(sock_listen, NULL, NULL);
-	printf("accepted at socket: %d",sock_conn);
+	*sock_conn = accept(*sock_listen, NULL, NULL);
+	printf("accepted at socket: %d",*sock_conn);
 	return 0;
 }
 
 int loginUser(MYSQL* conn, char username[20], char password[20]) {
 	char consult[120] = {};
-	int err;
 	MYSQL_RES* result;
 	MYSQL_ROW Pwd;
 	if (sprintf(consult, "SELECT Pwd FROM Users WHERE (Username = \'%s\');", username) < 0) {
@@ -78,11 +73,6 @@ int loginUser(MYSQL* conn, char username[20], char password[20]) {
 		if (mysql_query(conn, consult) < 0) {
 			return -1;
 		}
-	}
-	if (err != 0) {
-		printf("Error user not found %u %s\n",
-			mysql_errno(conn), mysql_error(conn));
-		return -1;
 	}
 	result = mysql_store_result(conn);
 	Pwd = mysql_fetch_row(result);
@@ -104,9 +94,8 @@ int addUser(MYSQL *conn, char username[20], char password[20], int age, char mai
 	return 0;
 }
 
-int getEmail(MYSQL* conn, char username[20], char mailOutput[30]) {
+int getEmail(MYSQL* conn, char username[20], const char * mailOutput) {
 	char consult[120] = {};
-	int err;
 	MYSQL_RES* result;
 	MYSQL_ROW Mail;
 	if (sprintf(consult, "SELECT Mail FROM Users WHERE (Username = \'%s\');", username) < 0) {
@@ -117,11 +106,6 @@ int getEmail(MYSQL* conn, char username[20], char mailOutput[30]) {
 			return -1;
 		}
 	}
-	if (err != 0) {
-		printf("Error user not found %u %s\n",
-			mysql_errno(conn), mysql_error(conn));
-		return -1;
-	}
 	result = mysql_store_result(conn);
 	Mail = mysql_fetch_row(result);
 	if (Mail == NULL)return -1;
@@ -131,7 +115,6 @@ int getEmail(MYSQL* conn, char username[20], char mailOutput[30]) {
 
 int setSpam(MYSQL* conn, char username[20], int spam) {
 	char consult[120] = {};
-	int err;
 	if (sprintf(consult, "Update Users SET spam=\'%d\' WHERE (Username = \'%s\');", !spam, username) < 0) {
 		return -1;
 	}
@@ -140,17 +123,11 @@ int setSpam(MYSQL* conn, char username[20], int spam) {
 			return -1;
 		}
 	}
-	if (err != 0) {
-		printf("Error user not found %u %s\n",
-			mysql_errno(conn), mysql_error(conn));
-		return -1;
-	}
 	return 0;
 }
 
 int getAge(MYSQL* conn, char username[20], int* ageOutput) {
 	char consult[120] = {};
-	int err;
 	MYSQL_RES* result;
 	MYSQL_ROW Age;
 	if (sprintf(consult, "SELECT Age FROM Users WHERE (Username = \'%s\');", username) < 0) {
@@ -161,15 +138,10 @@ int getAge(MYSQL* conn, char username[20], int* ageOutput) {
 			return -1;
 		}
 	}
-	if (err != 0) {
-		printf("Error user not found %u %s\n",
-			mysql_errno(conn), mysql_error(conn));
-		return -1;
-	}
 	result = mysql_store_result(conn);
 	Age = mysql_fetch_row(result);
 	if (Age == NULL)return -1;
-	ageOutput = atoi(Age[0]);
+	*ageOutput=(int)strtol(Age[0],(char **) NULL,10);
 	return 0;
 }
 
@@ -180,7 +152,7 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_adr;
 	char petition[512]={};
 	char answer[512]={};
-	MYSQL *conn;
+	MYSQL *conn = NULL;
 	
 	char user[20] = {};
 	char password[20] = {};
@@ -191,99 +163,99 @@ int main(int argc, char *argv[])
 	if (initMySQLServer(conn) < 0) {
 		printf("Error opening Mysql");
 		exit(1);
+	}else{
+        printf("Mysql opened");
 	}
 
-	if (startTCPServer(serv_adr,sock_conn, sock_listen, 13550) < 0) {
+	if (startTCPServer(&serv_adr,&sock_conn, &sock_listen, 13550) < 0) {
 		printf("Error opening tcp socket");
 		exit(1);
 	}
-	for (;;){
-		printf ("Escuchando\n");
-		
-		
-		printf ("connected\n");
-		
-		int stop =0;
-	
+	else{
+	    printf("connection secured");
+	}
 
-		while (stop ==0)
-		{
-			ret=read(sock_conn,petition, sizeof(petition));
-			if(ret<0){
-				printf("no data in buffer\n");
-			}
-			else{
-				printf ("received\n");
-				petition[ret]='\0';
-				
-				
-				printf ("Petition: %s\n",petition);
-				char *p = strtok( petition, "/");
-				char data[512] = {};
-				strcpy(data, p + 2);
-				int code =  atoi (p);
-				switch (code)
-				{
-				case 0:
-					stop = 1;
-					break;
-				case 1:
-					//LOGIN 0/user,password -->0/0 good 0/-1 NG
-					p = strtok(data, ",");
-					strcpy(user, p);
-					p = strtok(NULL, ",");
-					strcpy(password, p);
-					sprintf(answer, "1/%d",loginUser(conn,user,password));
-					break;
-				case 2:
-					//REGISTER 1/user,password,age,mail,spam -->1/0 good 1/-1 not good
-					p = strtok(data, ",");
-					strcpy(user, p);
-					p = strtok(NULL, ",");
-					strcpy(password, p);
-					p = strtok(NULL, ",");
-					age = atoi(p);
-					p = strtok(NULL, ",");
-					strcpy(mail, p);
-					p = strtok(NULL, ",");
-					spam = atoi(p);
-					sprintf(answer, "2/%d", addUser(conn, user, password,age,mail,spam));
-					break;
-				case 3:
-					//GET AGE 2/user -->2/age
-					p = strtok(data, ",");
-					strcpy(user, p);
-					getAge(conn, user, &age);
-					sprintf(answer, "3/%d",age);
-					break;
+		
+    int stop =0;
 
-				case 4:
-					//GET MAIL 3/user -->3/mail
-					p = strtok(data, ",");
-					strcpy(user, p);
-					getEmail(conn, user, mail);
-					sprintf(answer, "4/%s", mail);
-					break;
-				case 5:
-					//CHANGE SPAM 4/user,newspam -->4/0 changed 4/-1 not changed
-					p = strtok(data, ",");
-					strcpy(user, p);
-					p = strtok(NULL, ",");
-					spam = atoi(p);
-					sprintf(answer, "5/%d",setSpam(conn, user, spam));
-					break;
-				default:
-					//ERROR CODE
-					sprintf(answer, "-1/%d", code);
-				}
-				if (code !=0)
-				{
-					printf ("Answer: %s\n", answer);
-					// Send aswer
-					write (sock_conn,answer, strlen(answer));
-				}
-			}
-		}
+
+    while (stop ==0)
+    {
+        ret=read(sock_conn,petition, sizeof(petition));
+        if(ret<0){
+            printf("no data in buffer\n");
+        }
+        else{
+            printf ("received\n");
+            petition[ret]='\0';
+
+
+            printf ("Petition: %s\n",petition);
+            char *p = strtok( petition, "/");
+            char data[512] = {};
+            strcpy(data, p + 2);
+            int code =  (int)strtol(p,(char **) NULL,10);
+            switch (code)
+            {
+            case 0:
+                stop = 1;
+                break;
+            case 1:
+                //LOGIN 0/user,password -->0/0 good 0/-1 NG
+                p = strtok(data, ",");
+                strcpy(user, p);
+                p = strtok(NULL, ",");
+                strcpy(password, p);
+                sprintf(answer, "1/%d",loginUser(conn,user,password));
+                break;
+            case 2:
+                //REGISTER 1/user,password,age,mail,spam -->1/0 good 1/-1 not good
+                p = strtok(data, ",");
+                strcpy(user, p);
+                p = strtok(NULL, ",");
+                strcpy(password, p);
+                p = strtok(NULL, ",");
+                age = (int)strtol(p,(char **) NULL,10);
+                p = strtok(NULL, ",");
+                strcpy(mail, p);
+                p = strtok(NULL, ",");
+                spam = (int)strtol(p,(char **) NULL,10);
+                sprintf(answer, "2/%d", addUser(conn, user, password,age,mail,spam));
+                break;
+            case 3:
+                //GET AGE 2/user -->2/age
+                p = strtok(data, ",");
+                strcpy(user, p);
+                getAge(conn, user, &age);
+                sprintf(answer, "3/%d",age);
+                break;
+
+            case 4:
+                //GET MAIL 3/user -->3/mail
+                p = strtok(data, ",");
+                strcpy(user, p);
+                getEmail(conn, user, mail);
+                sprintf(answer, "4/%s", mail);
+                break;
+            case 5:
+                //CHANGE SPAM 4/user,newspam -->4/0 changed 4/-1 not changed
+                p = strtok(data, ",");
+                strcpy(user, p);
+                p = strtok(NULL, ",");
+                spam = (int)strtol(p,(char **) NULL,10);
+                sprintf(answer, "5/%d",setSpam(conn, user, spam));
+                break;
+            default:
+                //ERROR CODE
+                sprintf(answer, "-1/%d", code);
+            }
+            if (code !=0)
+            {
+                printf ("Answer: %s\n", answer);
+                // Send aswer
+                write (sock_conn,answer, strlen(answer));
+            }
+        }
 		// Server end
 		close(sock_conn); 
 	}
